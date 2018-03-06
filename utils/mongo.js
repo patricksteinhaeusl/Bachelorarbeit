49dd311bc47275e82f89f9a1b476a8a9a70cbc16
Steinhäusl Patrick <steinhaeusl.patrick@gmail.com>
2018-03-06 15:38:35 +0100

    - Mongoose Package 5.0.9 updated

--- utils/mongo.js
+++ utils/mongo.js
@@ -6,15 +6,27 @@ const Data = require('../data/index');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+mongoose.connect(GlobalConfig.mongo.connectionString());
 
-let util = mongoose.connect(GlobalConfig.mongo.connectionString(), {useMongoClient: true});
+let util = mongoose.connection;
 
-util.on('error', function () {
-    LogUtil.writeError('Database connection failed');
+util.on('disconnecting', function () {
+    LogUtil.writeError('Database disconnecting...');
 });
 
-util.once('open', function () {
-    LogUtil.writeInfo('Database connection successfully');
+util.on('disconnected', function () {
+    LogUtil.writeError('Database disconnected!');
+});
+
+util.on('connected', function () {
+    LogUtil.writeError('Database connected!');
+});
+
+util.on('connecting', function () {
+    LogUtil.writeError('Database connecting...');
+});
+
+util.once('open', function() {
     Data.drop(function () {
         Data.create(function () {
             LogUtil.writeInfo('Database prepared');
