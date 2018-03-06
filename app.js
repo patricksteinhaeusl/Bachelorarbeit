9730731793457f5e46f08f80902128aaed26da98
Steinhäusl Patrick <steinhaeusl.patrick@gmail.com>
2018-03-06 17:17:09 +0100

    - MongoDB Constant removed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const expressJwt = require('express-jwt');
 const path = require('path');
 
 const GlobalConfig = require('./configs/index');
-const MongoUtil = require('./utils/mongo');
+require('./utils/mongo');
 
 const auth = require('./routes/auth');
 const account = require('./routes/account');
