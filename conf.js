fc832866c91f1948de6d8209ad6f71658f82c634
Steinhäusl Patrick <steinhaeusl.patrick@gmail.com>
2018-03-13 16:10:29 +0100

    - Tests account, auth, post, nosqlinjection

--- conf.js
+++ conf.js
@@ -1,7 +1,7 @@
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['./tests/e2e/auth.js'],
+    specs: ['./tests/e2e/account.js'],
     capabilities: {
         browserName: 'chrome'
     }
