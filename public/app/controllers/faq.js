b07a59b61d3ac04647916037408f63f02901d2c6
Sven <sdefatsc@hsr.ch>
2018-05-10 17:37:28 +0200

    - finished vulnerability

--- public/app/controllers/faq.js
+++ public/app/controllers/faq.js
@@ -1,35 +1,43 @@
 'use strict';
 
-appControllers.controller('FaqController', ['FaqService',
-    function (faqService) {
+appControllers.controller('FaqController', ['$rootScope', '$scope', '$routeParams', 'FaqService',
+    function ($rootScope, $scope, $routeParams, faqService) {
         const self = this;
         self.data = {};
+        self.data.searchValue = null;
 
         self.init = function () {
             self.getFaq();
         };
 
         self.getFaq = function () {
-            faqService.getFaq(function (faqQuestions) {
-                self.data.FaqQuestions = faqQuestions;
+            $rootScope.messages = {};
+            faqService.getFaq(function (error, data, message, validations) {
+                if (error) $rootScope.messages.error = error;
+                if (validations) $rootScope.messages.validations = validations;
+                if (!data) $rootScope.messages.warning = message;
+                if (data) {
+                    self.data.FaqQuestions = data;
+                    $rootScope.messages.success = message;
+                }
             });
         };
 
         self.getFaqBySearchValue = function () {
-            faqService.getFaq(function (faqQuestions) {
-                self.data.FaqQuestions = faqQuestions;
-            });
+            if (self.data.searchValue) {
+                faqService.getFaqBySearchValue(self.data.searchValue, function (error, data, message, validations) {
+                    if (error) $rootScope.messages.error = error;
+                    if (validations) $rootScope.messages.validations = validations;
+                    if (!data) $rootScope.messages.warning = message;
+                    if (data) {
+                        self.data.FaqQuestions = data;
+                        $rootScope.messages.success = message;
+                    }
+                });
+            } else {
+                self.getFaq();
+            }
+            self.data.searchValue = '';
         };
         self.init();
-    }]).directive('ngEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if (event.which === 13) {
-                scope.$apply(function () {
-                    scope.$eval(attrs.ngEnter, {'event': event});
-                });
-                event.preventDefault();
-            }
-        });
-    };
-});
+    }]);
--- public/app/services/faq.js
+++ public/app/services/faq.js
@@ -1,26 +1,47 @@
 'use strict';
 
-appServices.factory('FaqService', ['$http', '$q', function ($http, $q) {
+appServices.factory('FaqService', ['$http', function ($http) {
     return {
         getFaq: function (callback) {
             $http
                 .get('/api/faq')
                 .then(function (response) {
-                    let faqQuestions = response.data.data.faq;
-                    return callback(faqQuestions);
-                }, function (response) {
-                    return callback(false);
+                    let statusCode = response.data.statusCode;
+                    let data = response.data.data;
+                    let message = response.data.message;
+                    let validations = response.data.validations;
+                    if (statusCode === 200) {
+                        return callback(null, data.faq, message, null);
+                    } else if (statusCode === 405) {
+                        return callback(null, null, null, validations);
+                    } else if (statusCode === 500) {
+                        return callback(message, null, null, null);
+                    }
+                    return callback(null, null, message, null);
+                }, function (error) {
+                    return callback(error);
                 });
         },
+
         getFaqBySearchValue: function (searchValue, callback) {
             let data = {searchValue: searchValue};
             $http
                 .post('/api/faq/searchValue/', data)
                 .then(function (response) {
-                    let faqQuestions = response.data.data.products;
-                    return callback(faqQuestions);
-                }, function (response) {
-                    return callback(false);
+                    let statusCode = response.data.statusCode;
+                    let data = response.data.data;
+                    let message = response.data.message;
+                    let validations = response.data.validations;
+                    if (statusCode === 200) {
+                        return callback(null, data.faq, message, null);
+                    } else if (statusCode === 405) {
+                        return callback(null, null, null, validations);
+                    } else if (statusCode === 500) {
+                        return callback(message, null, null, null);
+                    }
+                    return callback(null, null, message, null);
+                }, function (error) {
+                    return callback(error);
                 });
         },
     };
