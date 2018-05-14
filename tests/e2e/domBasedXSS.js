0066237cfdaf4899f57220ad4e15ae0de77925d3
Sven <sdefatsc@hsr.ch>
2018-05-14 14:23:10 +0200

    - changed URL for Test

--- tests/e2e/domBasedXSS.js
+++ tests/e2e/domBasedXSS.js
@@ -3,7 +3,7 @@
 describe('DOM Based XSS', function () {
     describe('should throw alert', function () {
         it('should be successfully', function () {
-            browser.get('https://localhost:3443/#!/shop?selectedQuantity=<script>alert("DomBasedXSS")</script>').then(function () {
+            browser.get(browser.params.webshop + '/#!/shop?selectedQuantity=<script>alert("DomBasedXSS")</script>').then(function () {
                 let alertDialog = browser.switchTo().alert();
                 expect(alertDialog.getText()).toBe('DomBasedXSS');
                 alertDialog.accept();
