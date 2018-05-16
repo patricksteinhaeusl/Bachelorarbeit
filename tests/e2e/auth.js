c40fff577628dcb28913d46b107b5ab161affcda
Sven <sdefatsc@hsr.ch>
2018-05-16 13:23:15 +0200

    - updated Auth Tests so they can be run without rebooting DB

--- tests/e2e/auth.js
+++ tests/e2e/auth.js
@@ -25,11 +25,10 @@ describe('Auth', function () {
     });
 
     describe('Registration', function () {
-
         beforeEach(function() {
             browser.get(browser.params.webshop);
         });
-
+        let randomNum = Math.floor(1000 + Math.random() * 9000);
         it('should success', function () {
             //Open Auth Menu
             element.all(by.css('.glyphicon.glyphicon-user')).get(0).click();
@@ -39,19 +38,21 @@ describe('Auth', function () {
             //Fill form
             element(by.model('auth.data.register.account.firstname')).sendKeys('Hans');
             element(by.model('auth.data.register.account.lastname')).sendKeys('Muster');
-            element(by.model('auth.data.register.account.email')).sendKeys('hans.muster@gmail.com');
-            element(by.model('auth.data.register.account.username')).sendKeys('hans_muster');
+            element(by.model('auth.data.register.account.email')).sendKeys('hans.muster'+randomNum+'@gmail.com');
+            element(by.model('auth.data.register.account.username')).sendKeys('hans_muster'+randomNum);
             element(by.model('auth.data.register.account.password')).sendKeys('customer250');
             //Submit form
             element(by.buttonText('Register')).click();
             browser.sleep(250);
             //Check
+            expect(element.all(by.className('alert alert-success alert-dismissible')).get(0).getText()).toBe("Success: Registration successfully\n×");
             expect(element.all(by.css('.menu-item-username')).get(0).getText()).toBe('Hans Muster');
         });
 
         it('should success', function () {
             HelperFunctions.logout(browser);
             //Check
+            expect(element.all(by.className('alert alert-success alert-dismissible')).get(0).getText()).toBe("Success: Logout successfully\n×");
             expect(element.all(by.css('.menu-item-username')).count()).toBe(0);
         });
 
